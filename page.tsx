f0202be328677e79e99840a2f96b82fc421bcb4a
ZuMoh19-Code <zunaidmohidin2025@gmail.com>
2026-06-03 18:48:15 +0200

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-import ExOCatalyst from './components/ExOCatalyst';
-
-export default function Home() {
-  return (
-    <main className="w-full overflow-x-hidden">
-      <ExOCatalyst />
-    </main>
-  );
-}
