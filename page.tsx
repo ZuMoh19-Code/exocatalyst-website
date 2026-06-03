3517214a9b0a915732a2c38fd11a8cbd4c5f334d
ZuMoh19-Code <zunaidmohidin2025@gmail.com>
2026-06-03 19:03:28 +0200

    app/page.tsx

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import ExOCatalyst from './components/ExOCatalyst';
+
+export default function Home() {
+  return (
+    <main className="w-full overflow-x-hidden">
+      <ExOCatalyst />
+    </main>
+  );
+}
